f7f1af680cc268e723127cdade23c3c95a6ff0a6
aait <info@aait.se>
2017-03-19 12:29:21 +0600

    Improved MobilePay Online method

--- Model/Method/MobilePay.php
+++ Model/Method/MobilePay.php
@@ -3,12 +3,13 @@
 namespace PayEx\Payments\Model\Method;
 
 use Magento\Framework\DataObject;
+use Magento\Framework\Exception\LocalizedException;
 
 /**
  * Class MasterPass
  * @package PayEx\Payments\Model\Method
  */
-class MobilePay extends \PayEx\Payments\Model\Method\AbstractMethod
+class MobilePay extends \PayEx\Payments\Model\Method\Cc
 {
 
     const METHOD_CODE = 'payex_mobilepay';
@@ -43,75 +44,9 @@ class MobilePay extends \PayEx\Payments\Model\Method\AbstractMethod
     protected $_canUseInternal = true;
     protected $_canUseCheckout = true;
     protected $_canUseForMultishipping = false;
-    protected $_isInitializeNeeded = false;
+    protected $_isInitializeNeeded = true;
     protected $_canFetchTransactionInfo = true;
 
-    /**
-     * Constructor
-     * @param \Magento\Framework\App\RequestInterface $request
-     * @param \Magento\Framework\UrlInterface $urlBuilder
-     * @param \PayEx\Payments\Helper\Data $payexHelper
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Framework\Locale\ResolverInterface $resolver
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
-     * @param \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory
-     * @param \Magento\Payment\Helper\Data $paymentData
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param \Magento\Payment\Model\Method\Logger $logger
-     * @param \Magento\Checkout\Model\Session $session
-     * @param \PayEx\Payments\Logger\Logger $payexLogger
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
-     * @param array $data
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     */
-    public function __construct(
-        \Magento\Framework\App\RequestInterface $request,
-        \Magento\Framework\UrlInterface $urlBuilder,
-        \PayEx\Payments\Helper\Data $payexHelper,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\Locale\ResolverInterface $resolver,
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
-        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
-        \Magento\Payment\Helper\Data $paymentData,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Payment\Model\Method\Logger $logger,
-        \Magento\Checkout\Model\Session $session,
-        \PayEx\Payments\Logger\Logger $payexLogger,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        parent::__construct(
-            $request,
-            $urlBuilder,
-            $payexHelper,
-            $storeManager,
-            $resolver,
-            $context,
-            $registry,
-            $extensionFactory,
-            $customAttributeFactory,
-            $paymentData,
-            $scopeConfig,
-            $logger,
-            $session,
-            $payexLogger,
-            $resource,
-            $resourceCollection,
-            $data
-        );
-
-        // Init PayEx Environment
-        $accountnumber = $this->getConfigData('accountnumber');
-        $encryptionkey = $this->getConfigData('encryptionkey');
-        $debug = (bool)$this->getConfigData('debug');
-        $this->payexHelper->getPx()->setEnvironment($accountnumber, $encryptionkey, $debug);
-    }
 
     /**
      * Method that will be executed instead of authorize or capture
@@ -121,47 +56,119 @@ class MobilePay extends \PayEx\Payments\Model\Method\AbstractMethod
      * @param object $stateObject
      *
      * @return $this
+     * @throws LocalizedException
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      * @api
      */
     public function initialize($paymentAction, $stateObject)
     {
-        /** @var \Magento\Sales\Model\Order\Payment $payment */
-        $payment = $this->getInfoInstance();
+        $this->payexLogger->info('initialize');
+
+        /** @var \Magento\Quote\Model\Quote\Payment $info */
+        $info = $this->getInfoInstance();
 
         /** @var \Magento\Sales\Model\Order $order */
-        $order = $payment->getOrder();
-        $order->setCanSendNewEmailFlag(false);
+        $order = $info->getOrder();
 
-        // Set Initial Order Status
-        $state = \Magento\Sales\Model\Order::STATE_NEW;
-        $stateObject->setState($state);
-        $stateObject->setStatus($state);
-        $stateObject->setIsNotified(false);
-    }
+        $order_id = $order->getIncrementId();
 
-    /**
-     * Get config payment action url
-     * Used to universalize payment actions when processing payment place
-     *
-     * @return string
-     * @api
-     */
-    public function getConfigPaymentAction()
-    {
-        $paymentAction = $this->getConfigData('payment_action');
-        return empty($paymentAction) ? true : $paymentAction;
-    }
+        // Get Currency code
+        $currency_code = $order->getOrderCurrency()->getCurrencyCode();
 
-    /**
-     * Checkout redirect URL getter for onepage checkout (hardcode)
-     *
-     * @see \Magento\Checkout\Controller\Onepage::savePaymentAction()
-     * @see \Magento\Quote\Model\Quote\Payment::getCheckoutRedirectUrl()
-     * @return string
-     */
-    public function getCheckoutRedirectUrl()
-    {
-        return $this->urlBuilder->getUrl('payex/mobilepay/redirect', ['_secure' => $this->request->isSecure()]);
+        // Get Operation Type (AUTHORIZATION / SALE)
+        $operation = $this->getConfigData('transactiontype');
+
+        // Get Additional Values
+        $additional = 'RESPONSIVE=1&USEMOBILEPAY=True';
+
+        // Language
+        $language = $this->getConfigData('language');
+        if (empty($language)) {
+            $language = $this->payexHelper->getLanguage();
+        }
+
+        // Get Amount
+        $amount = $order->getGrandTotal();
+
+        // Call PxOrder.Initialize8
+        $params = [
+            'accountNumber' => '',
+            'purchaseOperation' => $operation,
+            'price' => round($amount * 100),
+            'priceArgList' => '',
+            'currency' => $currency_code,
+            'vat' => 0,
+            'orderID' => $order_id,
+            'productNumber' => $order_id,
+            'description' => $this->payexHelper->getStore()->getName(),
+            'clientIPAddress' => $this->payexHelper->getRemoteAddr(),
+            'clientIdentifier' => 'USERAGENT=' . $this->request->getServer('HTTP_USER_AGENT'),
+            'additionalValues' => $additional,
+            'externalID' => '',
+            'returnUrl' => $this->urlBuilder->getUrl('payex/cc/success', [
+                '_secure' => $this->request->isSecure()
+            ]),
+            'view' => 'CREDITCARD',
+            'agreementRef' => '',
+            'cancelUrl' => $this->urlBuilder->getUrl('payex/cc/cancel', [
+                '_secure' => $this->request->isSecure()
+            ]),
+            'clientLanguage' => $language
+        ];
+        $result = $this->payexHelper->getPx()->Initialize8($params);
+        $this->payexLogger->info('PxOrder.Initialize8', $result);
+
+        // Check Errors
+        if ($result['code'] !== 'OK' || $result['description'] !== 'OK' || $result['errorCode'] !== 'OK') {
+            $message = $this->payexHelper->getVerboseErrorMessage($result);
+            throw new LocalizedException(__($message));
+        }
+
+        $order_ref = $result['orderRef'];
+        $redirectUrl = $result['redirectUrl'];
+
+        // Add Order Info
+        if ($this->getConfigData('checkoutinfo')) {
+            // Add Order Items
+            $items = $this->payexHelper->getOrderItems($order);
+            foreach ($items as $index => $item) {
+                // Call PxOrder.AddSingleOrderLine2
+                $params = [
+                    'accountNumber' => '',
+                    'orderRef' => $order_ref,
+                    'itemNumber' => ($index + 1),
+                    'itemDescription1' => $item['name'],
+                    'itemDescription2' => '',
+                    'itemDescription3' => '',
+                    'itemDescription4' => '',
+                    'itemDescription5' => '',
+                    'quantity' => $item['qty'],
+                    'amount' => (int)(100 * $item['price_with_tax']), //must include tax
+                    'vatPrice' => (int)(100 * $item['tax_price']),
+                    'vatPercent' => (int)(100 * $item['tax_percent'])
+                ];
+
+                $result = $this->payexHelper->getPx()->AddSingleOrderLine2($params);
+                $this->payexLogger->info('PxOrder.AddSingleOrderLine2', $result);
+            }
+
+            // Add Order Address Info
+            $params = array_merge([
+                'accountNumber' => '',
+                'orderRef' => $order_ref
+            ], $this->payexHelper->getAddressInfo($order));
+
+            $result = $this->payexHelper->getPx()->AddOrderAddress2($params);
+            $this->payexLogger->info('PxOrder.AddOrderAddress2', $result);
+        }
+
+        // Set Pending Payment status
+        $order->addStatusHistoryComment(__('The customer was redirected to PayEx.'), \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT);
+        $order->save();
+
+        // Save Redirect URL in Session
+        $this->session->setPayexRedirectUrl($redirectUrl);
+
+        return $this;
     }
 }
