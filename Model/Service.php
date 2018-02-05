6a1c6d5058ae479b1c44e6f2c4b8e7b6747c060e
Oleg Iskusnyh <oleg@iskusnyh.pro>
2018-02-05 15:33:53 +0600

    Editable option for Terms of Service. Close #59

--- Model/Service.php
+++ Model/Service.php
@@ -126,11 +126,19 @@ class Service implements ServiceInterface
      */
     public function get_service_terms($payment_method)
     {
+        // Get content
+        $content = $this->scopeConfig->getValue(
+            'payment/' . $payment_method .'/content_tos',
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()
+        );
+
         try {
             $block = $this->layout->createBlock('Magento\Framework\View\Element\Template');
             $block->setData('area', 'frontend')
-                  ->setTemplate('PayEx_Payments::checkout/terms_of_service.phtml')
-                  ->setData('method', $payment_method);
+                ->assign('method', $payment_method)
+                ->assign('content', $content)
+                ->setTemplate('PayEx_Payments::checkout/terms_of_service.phtml');
 
             return $block->toHtml();
         } catch (\Exception $e) {
